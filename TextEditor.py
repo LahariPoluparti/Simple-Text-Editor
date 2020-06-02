26d4bec885a0c976086461aec926ea411e82ca6b
[Lahari <[laharip@mail.usf.edu]>
2020-06-02 18:42:15 -0400

    Adding the main file

--- TextEditor.py
+++ TextEditor.py
@@ -1,16 +1,24 @@
 from tkinter import *
+from File import *
+from Edit import *
 
 #setting the window
 root = Tk()
 #sets the title to the center
 blank_space = " "
-root.title(blank_space * 110 + "Text Editor")
+root.title(blank_space * 110 + "Scribble")
 root.minsize(800,500) #the minimun size of the window
-root.maxsize(800,500) #the maximum size it can go
+#root.maxsize(800,500) #the maximum size it can go
+
+scroll = Scrollbar(root)
+scroll.pack(side=RIGHT, fill=Y)
 
 #textediting
-text_edit = Text(root, height=29.5, width = 100)
-text_edit.pack()
+text_edit = Text(root, undo=True)
+text_edit.pack(expand=True, fill=BOTH)
+
+file_option = File(root, text_edit)
+edit_option = Edit(root,text_edit)
 
 #Menu bar
 menu = Menu(root)
@@ -18,33 +26,26 @@ root.config(menu=menu)
 
 sub_menu = Menu(menu)
 menu.add_cascade(label="File", menu=sub_menu) #add_cascade -- dropdown
-sub_menu.add_command(label="New File") #add options to the file
-sub_menu.add_command(label="New Window")
-sub_menu.add_command(label="Open")
-sub_menu.add_command(label="Save")
-sub_menu.add_command(label="Save As")
+sub_menu.add_command(label="New File", command=file_option.new_file) #add options to the file
+sub_menu.add_command(label="Open", command=file_option.open_file)
+sub_menu.add_command(label="Save", command=file_option.save)
+sub_menu.add_command(label="Save As", command=file_option.save_as)
 sub_menu.add_separator()
-sub_menu.add_command(label="Exit")
+sub_menu.add_command(label="Exit", command=file_option.exit)
 
 edit_menu = Menu(menu)
 menu.add_cascade(label="Edit", menu=edit_menu) #add_cascade -- dropdown
-edit_menu.add_command(label="Cut") #add options to the file
-edit_menu.add_command(label="Copy")
-edit_menu.add_command(label="Paste")
-edit_menu.add_command(label="Replace")
+edit_menu.add_command(label="Undo", command=text_edit.edit_undo)
+edit_menu.add_command(label="Redo", command=text_edit.edit_redo)
+edit_menu.add_separator()
+edit_menu.add_command(label="Cut", command=edit_option.cut) #add options to the file
+edit_menu.add_command(label="Copy", command=edit_option.copy)
+edit_menu.add_command(label="Paste", command=edit_option.paste)
 
-format_menu = Menu(menu)
-menu.add_cascade(label="Format", menu=format_menu) #add_cascade -- dropdown
-format_menu.add_command(label="Font") #add options to the file
-
-help_menu = Menu(menu)
-menu.add_cascade(label="Help", menu=help_menu) #add_cascade -- dropdown
-help_menu.add_command(label="About") #add options to the file
 
 #status bar
-status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+status = Label(root, text="Welcome to Scribble...", bd=1, relief=SUNKEN, anchor=W)
 status.pack(side=BOTTOM, fill=X)
-
 
 
 root.mainloop()
